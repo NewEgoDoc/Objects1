936a0396d77101704823a84ce3830aab6819ce76
Juhan <juhan211@naver.com>
2022-03-31 22:43:19 +0900

    설계 개선하기: 자율성을 높히자

--- src/main/java/com/objects/java/theater/TicketSeller.java
+++ src/main/java/com/objects/java/theater/TicketSeller.java
@@ -1,10 +1,18 @@
 package com.objects.java.theater;
 
+import com.objects.java.theater.domain.Audience;
+import com.objects.java.theater.domain.Ticket;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public class TicketSeller {
     private TicketOffice ticketOffice;
+
+    public void sellTo(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+        audience.buy(ticket);
+    }
+
+
 }
--- src/main/java/com/objects/java/theater/domain/Invitation.java
+++ src/main/java/com/objects/java/theater/domain/Invitation.java
@@ -2,7 +2,8 @@ package com.objects.java.theater.domain;
 
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 public class Invitation {
-
-
+    private LocalDateTime when;
 }
